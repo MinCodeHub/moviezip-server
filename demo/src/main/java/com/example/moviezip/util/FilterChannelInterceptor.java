4b0cb3d27c03bd762d30068f75c99099874d7bd7
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2025-05-24 01:21:30 +0900

    [feat] FilterChannelInterceptor 코드 정리

--- demo/src/main/java/com/example/moviezip/util/FilterChannelInterceptor.java
+++ demo/src/main/java/com/example/moviezip/util/FilterChannelInterceptor.java
@@ -3,7 +3,6 @@ package com.example.moviezip.util;
 import com.example.moviezip.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.hadoop.shaded.org.apache.kerby.kerberos.provider.token.JwtUtil;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.lang.NonNull;
@@ -19,7 +18,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -29,58 +27,47 @@ public class FilterChannelInterceptor implements ChannelInterceptor {
     private final jwtUtil jwtutil;
 
     private final CustomUserDetailsService customUserDetailsService;
-
+    // websocket을 통해 들어온 요청이 처리 되기전 실행된다.
     @Override
     public Message<?> preSend(@NonNull Message<?> message, @NonNull MessageChannel channel) {
 
         //STOMP의 헤더에 직접 접근
-        StompHeaderAccessor headerAccessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+
+        //StompHeaderAccessor headerAccessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
         log.info(">>>>>> headerAccessor : {}", headerAccessor);
-        assert headerAccessor != null;
         log.info(">>>>> headAccessorHeaders : {}", headerAccessor.getCommand());
 
-        // CONNECT 또는 SEND 명령에 대해서만 처리
-        if (Objects.equals(headerAccessor.getCommand(), StompCommand.CONNECT)
-                || Objects.equals(headerAccessor.getCommand(), StompCommand.SEND)) {
-            // Authorization 헤더가 있는지 확인
-            String token = headerAccessor.getNativeHeader("Authorization") != null
-                    ? removeBrackets(headerAccessor.getNativeHeader("Authorization").get(0))
-                    : null;
-            // Authorization 헤더에서 토큰 추출
-            //String token = removeBrackets(String.valueOf(headerAccessor.getNativeHeader("Authorization")));
-            log.info(">>>>>> Token received : {}", token);
-            if (token != null) {
-                try {
-                    String userNameFromToken = jwtutil.extractUsername(token); // jwtUtil은 JWT 유틸리티 클래스
-                    UserDetails userDetails = customUserDetailsService.loadUserByUsername(userNameFromToken);
-                    // 토큰 유효성 검사
+        // CONNECT 명령에 대해서만 처리
+        if (StompCommand.CONNECT.equals(headerAccessor.getCommand())) {
+            String rawToken = headerAccessor.getFirstNativeHeader("Authorization");
 
-                    // Authentication 객체 생성 및 SecurityContext에 설정
-                    if(jwtutil.validateToken(token, userDetails)){
-                        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-                    }else{
-                        throw new RuntimeException("Invalid or expired token");  // 적절한 예외 처리
-                    }
-                    // 토큰을 사용하여 사용자 ID 추출
+            if (rawToken  == null || rawToken .isBlank()) {
+                throw new CustomException(ExceptionStatus.INVALID_TOKEN);
+            }
+            log.info("받은 토큰 = {}", rawToken );
+            String token = removeBrackets(rawToken);
+            try {
+
+                if (jwtutil.validateToken(token)) {  //토큰 검증
                     Long userId = jwtutil.extractUserId(token);
+                    String username = jwtutil.extractUsername(token);
+                    UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    log.info(">>>> UserDetails 조회 결과: {}", userDetails);
+                    log.info(">>>> 설정된 Principal: {}", authentication);
+                    headerAccessor.setUser(authentication); // WebSocket용 Principal 설정
 
-
-                    // WebSocket 헤더에 AccountId를 추가하여, 후속 메시지에서 사용할 수 있도록 함
-                    headerAccessor.addNativeHeader("UserId", String.valueOf(userId));
-                    log.info(">>>>>> AccountId added to header : {}", userId);
-
-                } catch (Exception e) {
-                    log.warn(">>>>> Authentication Failed in FilterChannelInterceptor : ", e);
-                    // 인증 실패 시 예외 처리 (예: 401 Unauthorized 응답 처리)
-                    throw new CustomException(ExceptionStatus.INVALID_TOKEN);
                 }
-            } else {
-                log.warn(">>>>>> Authorization header is missing");
+            } catch (Exception e) {
+                log.info("토큰 검증 실패 = {}", e.getMessage());
+                throw new CustomException(ExceptionStatus.INVALID_TOKEN);
             }
         }
+
+
         return message;
 
     }
@@ -97,7 +84,6 @@ public class FilterChannelInterceptor implements ChannelInterceptor {
             log.info("Token after removing brackets: {}", token);  // 대괄호 제거 후 토큰 로그
         }
 
-        // Remove "Bearer " prefix if it exists
         if (token.startsWith("Bearer ")) {
             token = token.substring(7); // "Bearer " (7 characters) 제거
             log.info("Token after removing Bearer: {}", token);  // Bearer 제거 후 토큰 로그
