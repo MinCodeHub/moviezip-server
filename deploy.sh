28d77a6d96d4ab26f8a0267ea5bb94c9a0d4d3ab
me_nyong <gjalsdud1030@naver.com>
2025-12-01 23:56:00 +0900

    [infra] deploy 수정

--- deploy.sh
+++ deploy.sh
@@ -13,21 +13,33 @@ EXIST_BLUE=$(docker ps --filter "name=${DOCKER_APP_NAME}-blue" --filter "status=
 if [ -z "$EXIST_BLUE" ]; then
     echo "Deploy Blue"
     docker rm -f ${DOCKER_APP_NAME}-blue || true
-    docker run -d --name ${DOCKER_APP_NAME}-blue -p 8080:8080 \
-        -v $(pwd)/${JAR_FILE}:/app/${JAR_FILE} \
-        eclipse-temurin:17-jdk \
-        java -jar /app/${JAR_FILE} \
-        >> ${LOG_DIR}/blue.log 2>&1
+    docker run -d --name moviezip-app-blue -p 8081:8080 \
+      -v $(pwd)/demo-0.0.1-SNAPSHOT.jar:/app/demo-0.0.1-SNAPSHOT.jar \
+      -e SPRING_PROFILES_ACTIVE=prod \
+      -e DB_URL=jdbc:oracle:thin:@dblab.dongduk.ac.kr:1521/orclpdb \
+      -e DB_USERNAME=ss240205 \
+      -e DB_PASSWORD=ss240205ss \
+      -e MONGODB_URI=mongodb://localhost:27017/movieZip \
+      -e REDIS_HOST=localhost \
+      -e REDIS_PORT=6379 \
+      eclipse-temurin:17-jdk \
+      java -jar /app/demo-0.0.1-SNAPSHOT.jar
     sleep 15
     docker rm -f ${DOCKER_APP_NAME}-green || true
 else
     echo "Deploy Green"
     docker rm -f ${DOCKER_APP_NAME}-green || true
-    docker run -d --name ${DOCKER_APP_NAME}-green -p 8081:8080 \
-        -v $(pwd)/${JAR_FILE}:/app/${JAR_FILE} \
-        eclipse-temurin:17-jdk \
-        java -jar /app/${JAR_FILE} \
-        >> ${LOG_DIR}/green.log 2>&1
+    docker run -d --name moviezip-app-green -p 8082:8080 \
+      -v $(pwd)/demo-0.0.1-SNAPSHOT.jar:/app/demo-0.0.1-SNAPSHOT.jar \
+      -e SPRING_PROFILES_ACTIVE=prod \
+      -e DB_URL=jdbc:oracle:thin:@dblab.dongduk.ac.kr:1521/orclpdb \
+      -e DB_USERNAME=ss240205 \
+      -e DB_PASSWORD=ss240205ss \
+      -e MONGODB_URI=mongodb://localhost:27017/movieZip \
+      -e REDIS_HOST=localhost \
+      -e REDIS_PORT=6379 \
+      eclipse-temurin:17-jdk \
+      java -jar /app/demo-0.0.1-SNAPSHOT.jar
     sleep 15
     docker rm -f ${DOCKER_APP_NAME}-blue || true
 fi
