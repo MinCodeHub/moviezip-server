a4b5791ac4be1f3e068ede77f02562598877bdb2
me_nyong <gjalsdud1030@naver.com>
2025-12-01 23:21:07 +0900

    [infra] deploy 수정

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,21 @@
 cd /home/ec2-user/app
 
 DOCKER_APP_NAME=spring
+JAR_FILE="demo-0.0.1-SNAPSHOT.jar"
 
 # blue 컨테이너 실행 여부 확인
-EXIST_BLUE=$(docker compose --project-name ${DOCKER_APP_NAME}-blue -f docker-compose-blue.yml ps | grep Up)
+EXIST_BLUE=$(docker ps --filter "name=${DOCKER_APP_NAME}-blue" --filter "status=running" -q)
 
 if [ -z "$EXIST_BLUE" ]; then
     echo "Deploy Blue"
-    docker compose --project-name ${DOCKER_APP_NAME}-blue -f docker-compose-blue.yml up -d --build
-    sleep 30
-    docker compose --project-name ${DOCKER_APP_NAME}-green -f docker-compose-green.yml down
+    docker run -d --name ${DOCKER_APP_NAME}-blue -p 8080:8080 -v $(pwd)/${JAR_FILE}:/app/${JAR_FILE} openjdk:17 java -jar /app/${JAR_FILE}
+    sleep 15
+    docker rm -f ${DOCKER_APP_NAME}-green || true
 else
     echo "Deploy Green"
-    docker compose --project-name ${DOCKER_APP_NAME}-green -f docker-compose-green.yml up -d --build
-    sleep 30
-    docker compose --project-name ${DOCKER_APP_NAME}-blue -f docker-compose-blue.yml down
+    docker run -d --name ${DOCKER_APP_NAME}-green -p 8080:8080 -v $(pwd)/${JAR_FILE}:/app/${JAR_FILE} openjdk:17 java -jar /app/${JAR_FILE}
+    sleep 15
+    docker rm -f ${DOCKER_APP_NAME}-blue || true
 fi
 
 # 사용하지 않는 이미지 삭제
