94115bdbeaa986ce0f4c006921becb3c03e8074a
me_nyong <gjalsdud1030@naver.com>
2025-12-01 22:46:08 +0900

    [infra] deploy 수정

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,19 @@ cd /home/ec2-user/app
 
 DOCKER_APP_NAME=spring
 
-
 # blue 컨테이너 실행 여부 확인
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
+EXIST_BLUE=$(docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
 
 if [ -z "$EXIST_BLUE" ]; then
     echo "Deploy Blue"
-    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+    docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
     sleep 30
-    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+    docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 else
     echo "Deploy Green"
-    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+    docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
     sleep 30
-    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+    docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
 fi
 
 # 사용하지 않는 이미지 삭제
